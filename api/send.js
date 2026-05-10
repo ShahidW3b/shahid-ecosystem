d90767a9c91d56a537f4c3e717d3d838c4ed9332
Shahabuddin Shahid <144619586+ShahidW3b@users.noreply.github.com>
2026-05-10 02:05:05 +0200

    Fix reciption Gmail address

--- api/send.js
+++ api/send.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
 
     await resend.emails.send({
       from: "ShahidW3b <contact@shahidw3b.com>",
-      to: "shahabshahid000@gamil.com",
+      to: "shahabshahid000@gmail.com",
       subject: `New Inquiry from ${name}`,
       html: `
         <div style="font-family:Arial;padding:30px;background:#0b1220;color:white;">
